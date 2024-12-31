94b86ad610685b9be8aa25449b44190788808d07
Ratish149 <ratish.shakya149@gmail.com>
2024-12-31 11:35:42 +0545

    asda

--- account/views.py
+++ account/views.py
@@ -16,6 +16,13 @@ class UserListView(APIView):
         serializer = CustomUserSerializer(users, many=True)
         return Response(serializer.data)
     
+    def post(self, request):
+        serializer = CustomUserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
 
 class LoginView(APIView):
     serializer_class = LoginSerializer
--- core/settings.py
+++ core/settings.py
@@ -82,13 +82,13 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
+"""DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
