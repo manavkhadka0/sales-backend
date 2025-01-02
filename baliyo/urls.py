369669129b8fe430ec71b2302db43df75877f93a
Ratish149 <ratish.shakya149@gmail.com>
2025-01-02 13:48:49 +0545

    asd

--- baliyo/urls.py
+++ baliyo/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ProjectListCreateView
+from .views import ProjectListCreateView, download_project_pdf
 
 urlpatterns = [
     path('projects/', ProjectListCreateView.as_view(), name='project-list-create'),
+    path('projects/<int:project_id>/download/', download_project_pdf, name='download-project-pdf'),
 ]
--- baliyo/views.py
+++ baliyo/views.py
@@ -1,10 +1,53 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from rest_framework import generics
+from django.utils.html import strip_tags, format_html
 from .models import Project
 from .serializers import ProjectSerializer
+from django.template.loader import render_to_string
+from django.http import HttpResponse
+from xhtml2pdf import pisa  # Import xhtml2pdf
+from django.utils.text import slugify  # Import slugify to create a safe filename
 
 # Create your views here.
 
 class ProjectListCreateView(generics.ListCreateAPIView):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
+
+def download_project_pdf(request, project_id):
+    project = get_object_or_404(Project, id=project_id)  # Fetch the project by ID
+    
+    # Prepare the context for rendering
+    context = {
+        'project': project,
+        'project_description': format_html(project.project_description),
+        'project_objective': format_html(project.project_objective),
+        'technical_requirements': format_html(project.technical_requirements),
+        'project_plan': format_html(project.project_plan),
+        'timeline': format_html(project.timeline),
+        'resource_allocation': format_html(project.resource_allocation),
+        'budget_breakdown': format_html(project.budget_breakdown),
+        'quality_assurance': format_html(project.quality_assurance),
+        'progess_report': format_html(project.progess_report),
+        'issues_and_resolution': format_html(project.issues_and_resolution),
+        'final_deliverables': format_html(project.final_deliverables),
+        'concept_design_url': request.build_absolute_uri(project.concept_design.url) if project.concept_design else None,
+        'technical_drawing_url': request.build_absolute_uri(project.technical_drawing.url) if project.technical_drawing else None,
+        'model_3d_url': request.build_absolute_uri(project.model_3d.url) if project.model_3d else None,
+        'supportive_documents_url': request.build_absolute_uri(project.supportive_documents.url) if project.supportive_documents else None,
+    }
+
+    html_string = render_to_string('project_pdf.html', context)  # Use the cleaned context
+    
+    # Create a safe filename based on the project name
+    safe_project_name = slugify(project.project_name)
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = f'attachment; filename="{safe_project_name}_project.pdf"'  # Dynamic filename
+
+    # Convert HTML to PDF using xhtml2pdf
+    pisa_status = pisa.CreatePDF(html_string, dest=response)  # Generate PDF
+
+    if pisa_status.err:
+        return HttpResponse('We had some errors <pre>' + html_string + '</pre>')  # Handle errors
+
+    return response
