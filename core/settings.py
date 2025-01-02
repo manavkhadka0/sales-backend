45c62449f5d4baf8bf7a9afa1351a3b56c9a3365
Ratish149 <ratish.shakya149@gmail.com>
2025-01-02 13:49:09 +0545

    dfd

--- core/settings.py
+++ core/settings.py
@@ -83,14 +83,14 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
+"""DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
+}"""
 
-"""DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'yachu_sales',
@@ -100,7 +100,7 @@ DATABASES = {
         'PORT': '',
     }
 
-}"""
+}
 
 
 # Password validation
