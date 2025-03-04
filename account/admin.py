d88c6fc6b9c2568d74de962735a2d44a6271131a
Ratish149 <ratish.shakya149@gmail.com>
2025-03-04 17:48:56 +0545

    dfd

--- account/admin.py
+++ account/admin.py
@@ -13,7 +13,7 @@ admin.site.register(Factory,ModelAdmin)
 
 class CustomUserAdmin(ModelAdmin):
     model = CustomUser
-    list_display = ('username','phone_number','distributor__name','franchise__name','role', 'total_orders')  # Add other fields as necessary
+    list_display = ('first_name','phone_number','distributor__name','franchise__name','role', 'total_orders')  # Add other fields as necessary
     list_filter = ('role','distributor__name')
     def total_orders(self, obj):
         return obj.orders.count()  # Count the related orders for the user
