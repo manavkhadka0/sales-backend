bfc90529a8072ad87991f39e1e144e083de0823d
Ratish149 <ratish.shakya149@gmail.com>
2025-05-11 16:00:48 +0545

    dfd

--- sales/views.py
+++ sales/views.py
@@ -345,7 +345,7 @@ class OrderListCreateView(generics.ListCreateAPIView):
                        rest_filters.SearchFilter, rest_filters.OrderingFilter]
     search_fields = ['phone_number', 'sales_person__username',
                      'delivery_address', 'full_name']
-    ordering_fields = ['-id']
+    ordering_fields = ['__all__']
     pagination_class = CustomPagination
     parser_classes = (JSONParser, FormParser, MultiPartParser)
 
