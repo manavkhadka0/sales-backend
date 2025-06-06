972bec91da1e6dcd39e70e3fbbeb090e0a9fe683
Ratish149 <ratish.shakya149@gmail.com>
2025-06-06 11:17:57 +0545

    dfd

--- sales/models.py
+++ sales/models.py
@@ -183,10 +183,11 @@ class Order(models.Model):
         max_digits=10, decimal_places=2, default=0, blank=True, null=True)
     delivery_type = models.CharField(
         max_length=255, choices=DELIVERY_ADDRESS_CHOICES, blank=True, null=True)
-    date = models.DateField(default=timezone.now)
+    date = models.DateField(default=timezone.now, null=True, blank=True)
     prepaid_amount = models.DecimalField(
         max_digits=10, decimal_places=2, default=0, blank=True, null=True)
-    created_at = models.DateTimeField(default=timezone.now)
+    created_at = models.DateTimeField(
+        default=timezone.now, null=True, blank=True)
     updated_at = models.DateTimeField(auto_now=True)
     total_amount = models.DecimalField(
         max_digits=10, decimal_places=2, default=0)
