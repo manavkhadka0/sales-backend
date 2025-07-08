d50764fb7db24cdf582515bb216acd7c85dd9287
Ratish149 <ratish.shakya149@gmail.com>
2025-07-08 17:40:19 +0545

    dfd

--- sales/views.py
+++ sales/views.py
@@ -3249,7 +3249,7 @@ class PackagingSentToDashSummaryCSVView(APIView):
                 address_parts.append(order.delivery_address)
             if getattr(order, "city", None):
                 address_parts.append(order.city)
-                full_address = ", ".join(address_parts)
+            full_address = ", ".join(address_parts)
             writer.writerow([
                 order.created_at.strftime('%Y-%m-%d %H:%M:%S'),
                 order.full_name,
