36d66ef33f3e8f168a8ca56314081800b8f2e9bd
Ratish149 <ratish.shakya149@gmail.com>
2025-09-14 17:11:52 +0545

    dfd

--- logistics/views.py
+++ logistics/views.py
@@ -165,7 +165,8 @@ def get_complete_dashboard_stats(request, franchise_id):
 
         # Helper function for payment stats
         def get_payment_stats(payment_method, statuses=None):
-            filtered = orders.filter(payment_method=payment_method)
+            filtered = orders.filter(
+                logistics='YDM', payment_method=payment_method)
             if statuses:
                 if isinstance(statuses, str):
                     statuses = [statuses]
@@ -219,8 +220,8 @@ def get_complete_dashboard_stats(request, franchise_id):
         data = {
             'overall_statistics': {
                 'Total Orders': get_status_stats(['Sent to YDM', 'Verified', 'Out For Delivery', 'Rescheduled', 'Delivered', 'Cancelled', 'Returned By Customer', 'Returned By YDM', 'Return Pending']),
-                'Total COD': get_payment_stats('Cash on Delivery', ['Sent to YDM', 'Verified', 'Out For Delivery', 'Rescheduled', 'Delivered']),
-                'Total RTV': get_status_stats(['Returned By Customer', 'Returned By YDM', 'Return Pending']),
+                'Total COD': get_status_stats(['Sent to YDM', 'Verified', 'Out For Delivery', 'Rescheduled', 'Delivered', 'Cancelled', 'Returned By Customer', 'Returned By YDM', 'Return Pending']),
+                'Total RTV': get_status_stats('Return Pending'),
                 'Total Delivery Charge': {
                     'nos': orders.count(),
                     'amount': float(orders.aggregate(total=Sum('delivery_charge'))['total'] or 0)
