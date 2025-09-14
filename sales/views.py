115a86568663c02cd9e93419029d9911bb23dc3b
Ratish149 <ratish.shakya149@gmail.com>
2025-09-14 15:19:55 +0545

    dfd

--- sales/views.py
+++ sales/views.py
@@ -468,7 +468,7 @@ class OrderListCreateView(generics.ListCreateAPIView):
         elif user.role == 'SuperAdmin':
             return Order.objects.filter(factory=user.factory).order_by('-id')
         elif user.role == 'Packaging':
-            return Order.objects.filter(franchise=user.franchise, order_status__in=['Pending', 'Processing', 'Sent to Dash', 'Delivered']).order_by('-id')
+            return Order.objects.filter(franchise=user.franchise, order_status__in=['Pending', 'Processing', 'Sent to Dash', 'Sent to YDM', 'Delivered']).order_by('-id')
         elif user.role == 'YDM_Rider':
             assigned_order_ids = AssignOrder.objects.filter(
                 user=user
