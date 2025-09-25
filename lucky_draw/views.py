3a0cc15d1a81f1e2bd23959cd10c96b27de12f03
Ratish149 <ratish.shakya149@gmail.com>
2025-09-25 16:06:39 +0545

    dfd

--- lucky_draw/views.py
+++ lucky_draw/views.py
@@ -129,11 +129,15 @@ class LuckyDrawSystemRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPI
 
     def destroy(self, request, *args, **kwargs):
         instance = self.get_object()
-        fest_config = FestConfig.objects.get_or_create(
+        
+        # Unpack get_or_create
+        fest_config, created = FestConfig.objects.get_or_create(
             franchise=instance.franchise,
         )
+        
         fest_config.lucky_draw_system = None
         fest_config.save()
+        
         self.perform_destroy(instance)
         return Response(status=status.HTTP_204_NO_CONTENT)
 
