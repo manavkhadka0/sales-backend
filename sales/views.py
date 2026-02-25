5073a4c33452e939648de45784ebdaf64d058fd4
Ratish149 <ratish.shakya149@gmail.com>
2026-02-25 17:15:43 +0545

    dfd

--- sales/views.py
+++ sales/views.py
@@ -366,7 +366,7 @@ class FranchiseInventoryListView(generics.ListAPIView):
 
 
 class CustomPagination(PageNumberPagination):
-    page_size = 10
+    page_size = 20
     page_size_query_param = "page_size"
     max_page_size = 100
 
