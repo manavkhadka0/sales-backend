842674035d299573274620cd67d3850ea3b36b1e
Ratish149 <ratish.shakya149@gmail.com>
2026-07-21 16:29:29 +0545

    chore: switch default database configuration from SQLite to PostgreSQL

--- core/settings.py
+++ core/settings.py
@@ -105,15 +105,15 @@ WSGI_APPLICATION = "core.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
+""" DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
     }
-}
+} """
 
 
-""" DATABASES = {
+DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": os.getenv("DB_NAME"),
@@ -122,7 +122,7 @@ DATABASES = {
         "HOST": os.getenv("DB_HOST"),
         "PORT": os.getenv("DB_PORT"),
     }
-} """
+}
 
 """ DATABASES = {
     "default": {
